fd8220ecab6bfb416cf60db1b1a698a8ddea8e37
Imam MAhbir Afraz <70149305+Afraz-2007@users.noreply.github.com>
2021-06-21 17:40:16 +0600

    updated covidcross

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+function dark() {
+
+    document.getElementById('body').style.backgroundColor = "#2f3640";
+    document.getElementById('body').style.color = "white";
+
+    if (document.getElementById('body').style.backgroundColor = "#2f3640"){
+        document.getElementById('mode1').style.display = "none";
+        document.getElementById('mode2').style.display = "block";
+    }
+}
+function light() {
+
+    document.getElementById('body').style.backgroundColor = "white";
+    document.getElementById('body').style.color = "#2f3640";
+
+    if (document.getElementById('body').style.backgroundColor = "white"){
+        document.getElementById('mode2').style.display = "none";
+        document.getElementById('mode1').style.display = "block";
+    }
+}
+
+if (document.getElementById('body').style.backgroundColor = "white"){
+    document.getElementById('mode2').style.display = "none";
+    document.getElementById('mode1').style.display = "block";
+}
+
